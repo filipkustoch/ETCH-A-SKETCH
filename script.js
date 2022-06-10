79f46123f6dc65528aa496966ea0e7f6579c305f
filipkustoch <filip.kustoch1@gmail.com>
2022-06-10 15:21:04 +0200

    first working

--- script.js
+++ script.js
@@ -5,19 +5,14 @@ function createGrid(rows, columns) {
     container.style.setProperty("--grid-columns", columns);
     for (var i = 0; i < (rows * columns); i++) {
         let item = document.createElement("div");
-        item.addEventListener("mouseover", function (event) {
-            // highlight the mouseenter target
-            event.target.style.backgroundColor = "purple";
-        
-        });
+        item.addEventListener("mouseover", ColorChange);
         container.appendChild(item).className = "grid-item";
     };
 
 };
 
-
-
-
-
-
+function ColorChange(event){
+    let color = document.getElementById("button-green").value;
+    event.target.style.backgroundColor = color;
+}
 createGrid(16, 16);
